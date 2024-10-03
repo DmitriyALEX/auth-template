377042d12ef2ee9bb29c3355fbe4f70076713fd6
DmitriyALEX <doiroffical@gmail.com>
2024-10-03 17:41:05 +0300

    configured privateKey in firebase admin

--- app/config/firebaseAdmin.js
+++ app/config/firebaseAdmin.js
@@ -1,12 +1,14 @@
 import admin from 'firebase-admin'
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
+
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
             type: process.env.FIREBASE_TYPE,
             project_id: process.env.FIREBASE_PROJECT_ID,
             private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-            private_key: process.env.FIREBASE_PRIVATE_KEY,
+            private_key: privateKey,
             client_email: process.env.FIREBASE_CLIENT_EMAIL,
             client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
             auth_uri: process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI,
