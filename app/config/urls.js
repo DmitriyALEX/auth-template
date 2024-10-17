6af35224f2b45421be35ebac422e43cec46b508c
DmitriyALEX <doiroffical@gmail.com>
2024-10-17 14:42:01 +0300

    fix: changed route

--- app/config/urls.js
+++ app/config/urls.js
@@ -1,27 +1,27 @@
-const BaseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_BASE_URL
-
-export const urls = {
-    //CHECK USER
-    checkUser: `${BaseUrl}/api/check-user`,
-    //CREATE OR UPDATE USERNAME
-    setUsername: `${BaseUrl}/api/set-username`,
-
-    //DELETE PROFILE
-    deleteUserProfile: `${BaseUrl}/api/delete-profile`,
-
-    //PUBLIC PROFILE
-    publicProfile: `${BaseUrl}/api/public-profile`,
-}
+// const BaseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_BASE_URL
 
 // export const urls = {
 //     //CHECK USER
-//     checkUser: '/api/check-user',
+//     checkUser: `${BaseUrl}/api/check-user`,
 //     //CREATE OR UPDATE USERNAME
-//     setUsername: '/api/set-username',
+//     setUsername: `${BaseUrl}/api/set-username`,
 
 //     //DELETE PROFILE
-//     deleteUserProfile: '/api/delete-profile',
+//     deleteUserProfile: `${BaseUrl}/api/delete-profile`,
 
 //     //PUBLIC PROFILE
-//     publicProfile: '/api/public-profile',
+//     publicProfile: `${BaseUrl}/api/public-profile`,
 // }
+
+export const urls = {
+    //CHECK USER
+    checkUser: '/api/check-user',
+    //CREATE OR UPDATE USERNAME
+    setUsername: '/api/set-username',
+
+    //DELETE PROFILE
+    deleteUserProfile: '/api/delete-profile',
+
+    //PUBLIC PROFILE
+    publicProfile: '/api/public-profile',
+}
